ffa143a695c348143a96510971e32681325c3a1e
Kyanid <kyanid.mail@gmail.com>
2026-02-14 01:41:32 +0100

    Ad

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -12,15 +12,6 @@ const slashLoader =
         ? slashLoaderImport.default
         : null
 
-const slashLoader =
-  typeof slashLoaderImport === "function"
-    ? slashLoaderImport
-    : typeof slashLoaderImport?.loadSlashCommands === "function"
-      ? slashLoaderImport.loadSlashCommands
-      : typeof slashLoaderImport?.default === "function"
-        ? slashLoaderImport.default
-        : null
-
 module.exports = client => {
   try {
     eventLoader(client)
--- src/commands/community/Reputation/meta.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  category: "Community",
-  description: "Reputation tools",
-  usage: "rep view|leaderboard",
-  example: "rep view 823131543610064907",
-}
